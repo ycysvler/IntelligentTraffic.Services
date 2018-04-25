82f01650594561f48fae718f08b1491a8fbf9b3c
ycysvler <ycysvler@hotmail.com>
2018-04-25 06:06:59 -0700

    * 增加卡口ID

--- server/mongo/dbschemas.js
+++ server/mongo/dbschemas.js
@@ -24,6 +24,7 @@ module.exports = class Schemas{
         this.imageSourceSchema = new mongoose.Schema({
             name: {type: String,index: {unique: true, dropDups: true}},       // 图片名称
             state:{type: Number,index: true},       //  状态 0:新图，1:正在计算特征，2：计算特征成功，-1：计算特征失败
+            kakouid:{type: String, index: true},    //  卡口ID
             kakou:Object,                           //  卡口信息（名称，经纬度等）
             source:Buffer,                          //  原始图像
             snaptime:Date,                          //  抓拍时间
