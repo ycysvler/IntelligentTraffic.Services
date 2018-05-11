a971568a38b056205bd20ab97cdaaa90e3f5b1ed
ycysvler <ycysvler@hotmail.com>
2018-05-10 20:51:41 -0700

    * ftp service

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,4 +1,4 @@
 /**
  * Created by yanggang on 2017/3/6.
  */
-module.exports = {port:6379,host:'192.168.31.123',password:'123456'};
+module.exports = {port:6379,host:'192.168.31.233',password:'123456'};
