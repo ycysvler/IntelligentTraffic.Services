ae2b8d98b51c6dee6b5e5f1b52ca7cb905dc98b1
VLER <ycysvler@hotmail.com>
2018-05-12 16:10:38 +0800

    * complete

--- server/pyas/vehicleservice.py
+++ server/pyas/vehicleservice.py
@@ -13,8 +13,8 @@ from IVehicleCalculator import vehicleMaster
 from flask import Flask,request ,Response
 
 
-#modelDir = r'/home/zhq/install_lib/vehicleDll/models'
-#master = vehicleMaster(modelDir,0,True,True,True)
+modelDir = r'/home/zhq/install_lib/vehicleDll/models'
+master = vehicleMaster(modelDir,0,True,True,True)
 
 # 用于json 序列化中处理numpy.float32类型
 class ComplexEncoder(json.JSONEncoder):
@@ -28,18 +28,38 @@ app = Flask(__name__)
 
 @app.route('/caculator')
 def caculator():
-    date = urllib.unquote(request.args.get("date"))
-    image =urllib.unquote(request.args.get("image"))
+    date = request.args.get("date")
+    image = request.args.get("image")
 
-    item = mongodb.db(date).imagesource.find_one({'name': image})
-    file = open('temp/' + item.name, 'wb')
-    file.write(item['source'])
-    file.close()
+    if (date is None):
+        return Response('[date] parameter is missing', status=403)
+    if (image is None):
+        return Response('[image] parameter is missing', status=403)
+    # 声明特征返回值
+    result = None
+    try:
+        # 数据库查找图片
+        item = mongodb.db(date).imagesource.find_one({'name': image})
+        if (item is None):
+            return Response( image + ' is missing', status=404)
+        # 写临时u图片到磁盘
+        imagepath = 'temp/' + item['name']
+        file = open(imagepath, 'wb')
+        file.write(item['source'])
+        file.close()
+        # 计算特征值
+        im = cv2.imread(imagepath)
+        result = master.detect(im)
+        # 删除临时图片
+        os.remove(imagepath)
+        # 修改图片处理状态
+        mongodb.db(date).imagesource.update({'name': image},{'$set':{'state':1}})
+    except(ex1):
+        return Response(str(ex1), status=500)
+    else:
+        print result
+        return Response(json.dumps(result, cls=ComplexEncoder),mimetype='application/json')
 
-    #r = t2(image)
-    #print r
-    return Response(json.dumps(item.name, cls=ComplexEncoder),mimetype='application/json')
-#    return str(r)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7777)
