27e6489ba52a3ba91e0eaf5b7f2138754dddf75c
VLER <ycysvler@hotmail.com>
2018-05-12 16:11:11 +0800

    * complete

--- server/pyas/test.py
+++ server/pyas/test.py
@@ -2,6 +2,9 @@ import mongodb
 
 if __name__ == "__main__":
     item = mongodb.db('19491001').imagesource.find_one({'name':'bbe459f0-503a-11e8-8f30-d5e4febeea38.jpg'})
+
+    mongodb.db('19491001').imagesource.update({'name': 'c34d7e50-538d-11e8-8934-8f5922d82a50.jpg'},{'$set':{'state':1}})
+
     if(item is None):
         print 'none'
     else:
