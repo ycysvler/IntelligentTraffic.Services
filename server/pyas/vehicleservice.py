087bd1c216498cb6c8515b00904af0a7a642e11d
VLER <ycysvler@hotmail.com>
2018-05-12 11:25:47 +0800

    * 增加python服务

--- server/pyas/vehicleservice.py
+++ server/pyas/vehicleservice.py
@@ -1,28 +1,40 @@
+#coding=utf-8
 import sys
-sys.path.append("./dll")
-from IVehicleCalculator import vehicleMaster
 import os
 import cv2
 import time
+import json
+import numpy
+import urllib
+sys.path.append("./dll")
+
+from IVehicleCalculator import vehicleMaster
+from flask import Flask,request ,Response
+
 
 modelDir = r'/home/zhq/install_lib/vehicleDll/models'
-master = vehicleMaster(modelDir,0,True,True,True)
+#master = vehicleMaster(modelDir,0,True,True,True)
 
-def run():
-    modelDir = r'/home/zhq/install_lib/vehicleDll/models'
-    master = vehicleMaster(modelDir,0,True,True,False)
-    picDir = r'/home/zhq/project/testPic/'
+# 用于json 序列化中处理numpy.float32类型
+class ComplexEncoder(json.JSONEncoder):
+  def default(self, obj):
+    if isinstance(obj, numpy.float32):
+      return round(float(obj),4)
+    else:
+      return json.JSONEncoder.default(self, obj)
 
-    for picName in os.listdir(picDir):
-        print picDir+picName
-        im = cv2.imread(picDir+picName)
-        start = time.time()
+app = Flask(__name__)
 
-        result = master.detect(im)
-	print result
-        end = time.time()
-
-        print "cost time: ",(end-start)*1000,"ms","*********",len(result)
+@app.route('/caculator')
+def caculator():
+    date = urllib.unquote(request.args.get("date"))
+    image =urllib.unquote(request.args.get("image"))
+    print date
+    print image
+    #r = t2(image)
+    #print r
+    return Response(json.dumps({'image':image,'date':date}, cls=ComplexEncoder),mimetype='application/json')
+#    return str(r)
 
 if __name__ == '__main__':
-    run()
+    app.run(host='0.0.0.0', port=7777)
