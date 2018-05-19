ed0ae1781a0387335e995d136343ecd80bc32404
VLER <ycysvler@hotmail.com>
2018-05-19 11:42:44 +0800

    + 测试批量计算车型

--- server/pyas/test1.py
+++ server/pyas/test1.py
@@ -11,8 +11,21 @@ from IVehicleCalculator import vehicleMaster
 modelDir = r'/home/zhq/install_lib/vehicleDll/models'
 master = vehicleMaster(modelDir,0,True,True,False)
 
+def adapterAnalysis(dbdate, snaptime, name, kakouid, vehicle ):
+    analysis = mongodb.db(dbdate).analysis
+    item = {}
+    item['name'] = name
+    item['kakouid'] = kakouid
+    item['vehiclezone'] = {
+        "x": vehicle['vehicleZone'][0],
+        "y": vehicle['vehicleZone'][1],
+        "width": vehicle['vehicleZone'][2] - vehicle['vehicleZone'][0],
+        "height": vehicle['vehicleZone'][3] - vehicle['vehicleZone'][1]
+    }
+
 def run():
     date = '20170427'
+    i = 0
     for item in mongodb.db(date).imagesource.find():
         start = time.time()
         imagepath = 'temp/' + item['name']
@@ -26,7 +39,11 @@ def run():
         os.remove(imagepath)
         end = time.time()
 
+        adapterAnalysis(date, '', item.name, item.kakouid, result)
         print "cost time: ", (end - start) * 1000, "ms", "*********", item['name']
+        i=i+1
+        if(i>3):
+            return
 
 if __name__ == '__main__':
     run()
