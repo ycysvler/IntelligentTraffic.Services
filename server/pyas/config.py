4023654c3aaf81c4b052b3344e6e4cce1faf345d
ycysvler <ycysvler@hotmail.com>
2018-06-22 20:53:16 -0700

    up

--- server/pyas/config.py
+++ server/pyas/config.py
@@ -3,6 +3,6 @@
 
 mysql = {'host':'192.168.31.200', 'user':'root', 'passwd':'root'}
 
-mongodb = {'host':'192.168.31.34', 'port':27017}
+mongodb = {'host':'192.168.31.200', 'port':27017}
 
-ftp = {'host':'192.168.31.200', 'user':'ftp', 'passwd':'ftp'}
+ftp = {'host':'192.168.31.200', 'user':'ftp', 'passwd':'ftp','descode':'gbk'}
