a5c575d286a1733275697e30e2cd0038fbd7d980
ycysvler <ycysvler@hotmail.com>
2018-07-05 07:15:42 -0700

    up

--- server/pyas/ftp.py
+++ server/pyas/ftp.py
@@ -39,8 +39,32 @@ class MyFTP:
         self.ftp.login( user, passwd ) 
         logger.info({"content":'ftp host=%s username=%s password=%s'%(self.host, user, passwd)})    
         logger.info({"content":self.ftp.welcome})
-  
-    def DownLoadByDate(self):         
+
+    def getSubdir(self, *args):
+        '''拷贝了 nlst() 和 dir() 代码修改，返回详细信息而不打印'''
+        cmd = 'LIST'
+        func = None
+        if args[-1:] and type(args[-1]) != type(''):
+            args, func = args[:-1], args[-1]
+        for arg in args:
+            cmd = cmd + (' ' + arg)
+        files = []
+        self.ftp.retrlines(cmd, files.append)
+        return files
+
+    def getdirs(self, dirname=None):
+        """返回目录列表，包括文件简要信息"""
+        if dirname != None:
+            self.cwd(dirname)
+        files = self.getSubdir()
+
+        # 处理返回结果，只需要目录名称
+        r_files = [file.split(" ")[-1] for file in files]
+
+        # 去除. .. 
+        return [file for file in r_files if file != "." and file !=".."]
+
+    def DownLoadByDate(self):  
         LocalDir = 'image/' + self.date + '/'
         RemoteDir = '/image/' + self.date + '/'
 
@@ -51,7 +75,7 @@ class MyFTP:
 
         try:        
             self.ftp.cwd(RemoteDir)
-            lst = self.ftp.nlst()
+            lst = self.getdirs()
             for item in lst: 
                 # terrible chinese, terrible gbk
                 file = item.decode(self.descode,'strict')
